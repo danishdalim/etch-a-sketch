7115441e400326f0dcda576c5259f501399cf93c
Danish <danishprogram@gmail.com>
2022-09-07 21:01:49 +0800

    Make functional etch-a-sketch

---
+++ javascript.js
@@ -0,0 +1,29 @@
+function populateBoard(size) {
+let board = document.querySelector(".board");
+let squares = board.querySelectorAll('div');
+squares.forEach((div) => div.remove());
+board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+
+let amount = size*size
+for(let i=0; i<amount; i++){
+    let square = document.createElement('div');
+    square.addEventListener('mouseover',()=> {
+        square.style.backgroundColor = 'white';
+    })
+    square.style.backgroundColor = 'black';
+    board.insertAdjacentElement("beforeend",square);
+}
+}
+
+populateBoard(16);
+
+function changeSize(input){
+    if (input >=2 && input <= 100){
+        populateBoard(input);
+    } else {
+        return console.log('error. Too many Squares');
+    }
+    
+}
